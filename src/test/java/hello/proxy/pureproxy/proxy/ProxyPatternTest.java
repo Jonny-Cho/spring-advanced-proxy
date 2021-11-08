584edb26bdc8d9e022466784edfe3963a5cae3ea
Jonny-Cho <andante2183@gmail.com>
2021-11-08 21:40:45 +0900

    step4: 프록시 패턴 - 예제 코드1

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,18 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        final RealSubject realSubject = new RealSubject();
+        final ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+}
