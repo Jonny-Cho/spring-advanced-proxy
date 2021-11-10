284e16a5bb3aab99fc1efddfda3d4b308c2f2720
Jonny-Cho <andante2183@gmail.com>
2021-11-10 23:41:49 +0900

    step4: 데코레이터 패턴 - 예제 코드2

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component {
+
+    private Component component;
+
+    public MessageDecorator(final Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+        final String result = component.operation();
+        final String decoResult = "*****" + result + "*****";
+        log.info("적용 전={}, 적용 후={}", result, decoResult);
+        return decoResult;
+    }
+}
